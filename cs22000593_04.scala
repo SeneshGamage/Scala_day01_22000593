afa790781e0e1bc1870814e6d4063bdf446b4977
SeneshGamage <140165794+SeneshGamage@users.noreply.github.com>
2024-06-18 09:35:48 +0530

    Add files via upload

---
+++ cs22000593_04.scala
@@ -0,0 +1,28 @@
+package Scala
+
+object cs22000593_04 {
+  def main(args:Array[String]){
+    def calculateDiscountedPrice(coverPrice: Double, discount: Double): Double = {
+  coverPrice * (1 - discount)
+}
+
+def calculateShippingCost(totalUnits: Int): Double = {
+  val firstCopyShipping = 3.0
+  val additionalCopyShipping = 0.75
+  firstCopyShipping + (additionalCopyShipping * (totalUnits - 1))
+}
+
+def calculateTotalWholesaleCost(totalUnits: Int): Double = {
+  val coverPrice = 24.95
+  val discount = 0.4
+  val discountedPrice = calculateDiscountedPrice(coverPrice, discount)
+  val shippingCost = calculateShippingCost(totalUnits)
+  discountedPrice * totalUnits + shippingCost
+}
+
+// Usage
+val totalUnits = 60
+val totalCost = calculateTotalWholesaleCost(totalUnits)
+println(s"The total wholesale cost for $totalUnits copies is Rs. $totalCost")
+  }
+}
